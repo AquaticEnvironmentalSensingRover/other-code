13974d5f0687bd40165dda1dbb27fb894b4b5326
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-06-10 19:51:53 -0400

    Adding Adafruit ADS1115 test read

---
+++ ads1115_read.py
@@ -0,0 +1,24 @@
+import smbus
+
+
+BUS = 1		 	# Which smbus to use
+ADDRESS = 0X48 	# Address of the device
+
+
+BUS = SMBUS.SMBUS(BUS)
+
+
+# Read two bytes from register 01, the config register
+config = bus.read_word_data(ADDRESS, 0X01) & 0xFFFF
+print('Old config value: 0x{0:04X}'.format(config))
+
+
+# Write two bytes to the config register
+new_config = 0b0100001110000011
+bus.write_word_data(ADDRESS, 0x01, new_config)
+
+# Read two bytes from register 00, the ADC value
+value = bus.read_word_data(ADDRESS, 0x00) & 0xFFFF
+# Swap byte order from little endian to big endian
+value = ((value & 0xFF) << 8) | (value >> 8)
+print('Raw ADC value: 0x{0:04X}'.format(value))
