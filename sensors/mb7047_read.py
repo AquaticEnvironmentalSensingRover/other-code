b21e7559eea64ad763e17316cbeed110265ae47c
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-06-17 18:08:22 -0400

    Add MaxBotics 7047 read

---
+++ sensors/mb7047_read.py
@@ -0,0 +1,12 @@
+import smbus
+import time
+
+REG_START_WRITE = 81    # register address to take a measurement
+REG_START_MEASURE = 0   # register address to read back measurement
+ADDRESS = 0x70
+
+bus = smbus.SMBus(1)
+
+bus.write_byte_data(ADDRESS, REG_START_WRITE, 0)
+time.sleep(0.5)
+bus.read_word_data(ADDRESS, REG_START_MEASURE)
