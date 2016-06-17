cbc3b48041a067e3eb8ddb9ceb002a4020117e45
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-06-17 18:35:41 -0400

    Fix IOError in MB7047 bus write

--- sensors/mb7047_read.py
+++ sensors/mb7047_read.py
@@ -7,6 +7,6 @@ ADDRESS = 0x70
 
 bus = smbus.SMBus(1)
 
-bus.write_byte_data(ADDRESS, REG_START_WRITE, 0)
+bus.write_byte(ADDRESS, REG_START_WRITE)
 time.sleep(0.5)
 bus.read_word_data(ADDRESS, REG_START_MEASURE)
