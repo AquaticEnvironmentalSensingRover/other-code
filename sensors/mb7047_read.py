ee9ed5674827218ea93c9a6913444d755bfb8221
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-06-17 19:45:20 -0400

    Fix MB7047 read
    Before this fix, it would print out a value that was incorrect.

--- sensors/mb7047_read.py
+++ sensors/mb7047_read.py
@@ -1,12 +1,23 @@
 import smbus
 import time
 
-REG_START_WRITE = 81    # register address to take a measurement
-REG_START_MEASURE = 0   # register address to read back measurement
-ADDRESS = 0x70
+ADDRESS = 0x70          # Device address
+REG_START_WRITE = 81    # Register address to take a measurement
+REG_START_MEASURE = 0   # Register address to read back measurement
 
-bus = smbus.SMBus(1)
+bus = smbus.SMBus(1)    # Create bus
 
+# Signal device to take measurement
 bus.write_byte(ADDRESS, REG_START_WRITE)
-time.sleep(0.5)
-print(bus.read_word_data(ADDRESS, REG_START_MEASURE))
+
+# Wait for measurement to be acquired
+time.sleep(0.1)
+
+# Read measurement data from device
+data = bus.read_i2c_block_data(ADDRESS, REG_START_MEASURE)
+
+# Remove leading bit from first byte
+newData = [data[0] & 0b1111111, data[1]]
+
+# Format and print data
+print(str(newData) + " | " + str((newData[0]<<8) + newData[1]))
